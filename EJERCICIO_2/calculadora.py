4c6f8910e719548ff4167a817a9768f14d51580b
lop326 <lopezalvaroalberto326@gmail.com>
2024-07-24 17:52:05 -0300

    se verifica la division por cero

--- EJERCICIO_2/calculadora.py
+++ EJERCICIO_2/calculadora.py
@@ -8,7 +8,11 @@ def multiplicacion(num1,num2):
     print(f"{num1} * {num2} = {num1*num2}")
     
 def division(num1,num2):
-    print(f"{num1} / {num2} = {num1/num2}")
+    
+    if num2 == 0:
+        print("la division por cero no existe")
+    else:
+        print(f"{num1} / {num2} = {num1/num2}")
     
 
 
